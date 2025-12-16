fd8b89f6102d8b3c11d5aa19434b02e1479e1a64
AdityaChaple15 <adityachaple02>
2025-12-17 00:44:56 +0530

    Added password hashing using bcrypt

--- src/main/java/com/example/UberProject_AuthService/controllers/AuthController.java
+++ src/main/java/com/example/UberProject_AuthService/controllers/AuthController.java
@@ -2,19 +2,35 @@ package com.example.UberProject_AuthService.controllers;
 
 import com.example.UberProject_AuthService.dto.PassengerDto;
 import com.example.UberProject_AuthService.dto.PassengerSignupRequestDto;
+import com.example.UberProject_AuthService.models.Passenger;
+import com.example.UberProject_AuthService.services.AuthService;
+import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthController {
 
-    @PostMapping("/signup/passenger")
-    private ResponseEntity<PassengerDto> signUp(@RequestBody PassengerSignupRequestDto passengerSignupRequestDto){
+    private AuthService authService;
 
-        return null;
+    public AuthController(AuthService authService){
+        this.authService = authService;
+    }
+
+    @PostMapping("/signup/passenger")
+    public ResponseEntity<PassengerDto> signUp(@RequestBody PassengerSignupRequestDto passengerSignupRequestDto){
+        PassengerDto response = authService.signupPassenger(passengerSignupRequestDto);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/signIn/passenger")
+    public ResponseEntity<?> signIn(){
+
+        return new ResponseEntity<>(10,HttpStatus.CREATED);
     }
 }
--- src/main/java/com/example/UberProject_AuthService/dto/PassengerDto.java
+++ src/main/java/com/example/UberProject_AuthService/dto/PassengerDto.java
@@ -1,5 +1,6 @@
 package com.example.UberProject_AuthService.dto;
 
+import com.example.UberProject_AuthService.models.Passenger;
 import lombok.*;
 
 import java.util.Date;
@@ -17,4 +18,16 @@ public class PassengerDto {
     private String phoneNumber;
     private String email;
     private Date createdAt;
+
+    public static PassengerDto from(Passenger p){
+        PassengerDto result = PassengerDto.builder()
+                .id(p.getId().toString())
+                .createdAt(p.getCreatedAt())
+                .email(p.getEmail())
+                .password(p.getPassword())
+                .name(p.getName())
+                .phoneNumber(p.getPhoneNumber())
+                .build();
+        return result;
+    }
 }
